d449588ff026d1acbf094605d1b322b10b44ecef
Michael Trumbull <W0304263@campus.nscc.ca>
2024-01-17 14:25:59 -0400

    Adding mmodel and dbUtility classes.

---
+++ Pets/Data/DbContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Pets.Data
+{
+    //
+    // This is a utility class to execute database CRUD function
+    //
+    public class DbContext
+    {
+    }
+}
---
+++ Pets/Models/Pet.cs
@@ -0,0 +1,13 @@
+﻿namespace Pets.Models
+{
+    //
+    // This is the Pet model, meant to store data in the Pets table in my database.
+    //
+    public class Pet
+    {
+        public int PetId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string PhotoFileName { get; set; } = string.Empty;
+    }
+}
