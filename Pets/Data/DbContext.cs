6bc1087fa70f820d830f9100ce56f73563be5be3
Michael Trumbull <W0304263@campus.nscc.ca>
2024-01-18 11:48:02 -0400

    Adding ADO.NET GetAllPets() functionality.

--- Pets/Data/DbContext.cs
+++ Pets/Data/DbContext.cs
@@ -1,9 +1,60 @@
-﻿namespace Pets.Data
+﻿using Microsoft.Data.Sqlite;
+using Pets.Models;
+
+namespace Pets.Data
 {
     //
     // This is a utility class to execute database CRUD function
     //
-    public class DbContext
+    public static class DbContext
     {
+
+        public static List<Pet> GetAllPets()
+        {
+            List<Pet> pets = new List<Pet> ();
+
+            //
+            // Query the database to get the pets
+            //
+
+            // Create connection to database
+            SqliteConnection connection = new SqliteConnection("Data Source= Data/database.db");
+
+            // Open the connection
+            connection.Open();
+
+            // Run SQL to get all the pets
+            string sql = "SELECT PetId, Name, Age, PhotoFileName from Pet";
+
+            // Create a "command" (sql to execute in database) to execute SQL
+            SqliteCommand cmd = connection.CreateCommand ();
+            cmd.CommandText = sql;
+
+            // Execute the command (use a Data Reader so we can read() the rows returned by the command)
+            SqliteDataReader reader = cmd.ExecuteReader ();
+
+            // Read through the results
+            while(reader.Read ())
+            {
+                // For each database row, create a Pet object and add to the Pet list
+                Pet pet = new Pet ();
+                pet.PetId = reader.GetInt32(0);
+                pet.Name = reader.GetString(1);
+                pet.Age = reader.GetInt32(2);
+                if (!reader.IsDBNull(3))
+                {
+                    pet.PhotoFileName = reader.GetString(3);
+                }
+
+                // Add this pet to list
+                pets.Add(pet);
+            }
+
+            // Close the connection
+            connection.Close();
+
+            return pets;
+        }
+
     }
 }
--- Pets/Pages/Index.cshtml.cs
+++ Pets/Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Pets.Data;
+using Pets.Models;
 
 namespace Pets.Pages
 {
@@ -7,14 +9,16 @@ namespace Pets.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        public List<Pet> pets = new();
+
         public IndexModel(ILogger<IndexModel> logger)
         {
-            _logger = logger;
+            _logger = logger;            
         }
 
         public void OnGet()
         {
-
+            pets = DbContext.GetAllPets();
         }
     }
 }
